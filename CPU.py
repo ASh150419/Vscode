04bdefbae91f34970031c74ccf74d6089c83d6a4
ARTEM SH <sccc940bf@gmail.com>
2022-09-04 17:22:43 +0300

    Names

--- CPU.py
+++ CPU.py
@@ -1,14 +1,14 @@
 import psutil
 
 
-def _CPU_():
+def CPU_info():
     summcpu = {}
     cpu_data = psutil.cpu_times()
     summcpu.update(user_time=cpu_data.user, system_time=cpu_data.system, nice_time=cpu_data.nice, idle_time=cpu_data.idle)
     return summcpu
 
 
-def _Memory_():
+def Memory_info():
     summmem = {}
     memory_data = psutil.virtual_memory()
     summmem.update(total_mem=memory_data.total // 1E+6, available_mem=memory_data.available // 1E+6,
@@ -16,7 +16,7 @@ def _Memory_():
     return summmem
 
 
-def _Disk_():
+def Disk_info():
     summdisk = {}
     disk_data = psutil.disk_usage('/')
     summdisk.update(total_disk=disk_data.total // 1E+6, used_disk=disk_data.used // 1E+6,
@@ -25,40 +25,40 @@ def _Disk_():
 
 
 def show(cpu=None, mem=None, disk=None):
-    cpu_sh = "/// {:<38} /// {:^21} /// {:^22} /// {:>13} ///"
-    cpu_template = "/// {user_time} /// {system_time} /// {nice_time} /// {idle_time} ///"
-    cpu_end = "{}"
-    mem_sh = "\\\\\\\ {:<28} \\\\\\\ {:^24} \\\\\\\ {:^24} \\\\\\\ {:>33} "
-    memory_template = "\\\\\\\ {total_mem} \\\\\\\ {available_mem} \\\\\\\ {used_mem} \\\\\\ {cached_mem} \\\\\\\ "
-    mem_end = "{}"
+    cpu_template = "/// {:<38} /// {:^21} /// {:^22} /// {:>13} ///"
+    cpu_template1 = "/// {user_time} /// {system_time} /// {nice_time} /// {idle_time} ///"
+    cpu_template2 = "{}"
+    mem_template = "\\\\\\\ {:<28} \\\\\\\ {:^24} \\\\\\\ {:^24} \\\\\\\ {:>33} "
+    memory_template1 = "\\\\\\\ {total_mem} \\\\\\\ {available_mem} \\\\\\\ {used_mem} \\\\\\ {cached_mem} \\\\\\\ "
+    mem_template2 = "{}"
     disk_template = "///// {total_disk} ///// {used_disk} ///// {free_disk} ///// {percent_disk} /////"
-    disk_sh ="///// {:>22} ///// {:^23} ///// {:^20} ///// {:<16} /////"
-    disk_end = "{}"
-    print(cpu_sh.format("Время затрачиваемое обычными процессами", "Процессы в режиме ядра", "Проприетарные процессы", "Время простоя")
+    disk_template1 ="///// {:>22} ///// {:^23} ///// {:^20} ///// {:<16} /////"
+    disk_template2 = "{}"
+    print(cpu_template.format("Время затрачиваемое обычными процессами", "Процессы в режиме ядра", "Проприетарные процессы", "Время простоя")
     )
-    print(cpu_template.format(**cpu)
+    print(cpu_template1.format(**cpu)
     )
-    print(cpu_end.format("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+    print(cpu_template2.format("<" * 120)
     )
-    print(mem_sh.format("Общая физическая память (MB)", "Память под процессы (MB)" ,"Используемая память (MB)" ,"Неиспользуемая память вообще (MB)")
+    print(mem_template.format("Общая физическая память (MB)", "Память под процессы (MB)" ,"Используемая память (MB)" ,"Неиспользуемая память вообще (MB)")
     )        
-    print(memory_template.format(**mem)
+    print(memory_template1.format(**mem)
     )
-    print(mem_end.format(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    print(mem_template2.format(">" * 120)
     )
-    print(disk_sh.format("Общий объём диска (MB)", "Используемый объём (MB)", "Свободный объём (MB)" ,"Использование(%)")
+    print(disk_template1.format("Общий объём диска (MB)", "Используемый объём (MB)", "Свободный объём (MB)" ,"Использование(%)")
     )
     print(disk_template.format(**disk)
     )
-    print(disk_end.format("====================================================================================================================")
+    print(disk_template2.format("=" * 120)
     )
 
 
 def main():
-    _cpu_ = _CPU_()
-    _memory_ = _Memory_()
-    _disk_ = _Disk_()
-    show(cpu=_cpu_,mem=_memory_, disk=_disk_)
+    cpu_main = CPU_info()
+    memory_main = Memory_info()
+    disk_main = Disk_info()
+    show(cpu=cpu_main, mem=memory_main, disk=disk_main)
 
 
 if __name__ == "__main__":
